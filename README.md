2361af50ded9139f1093e507f8ea6493c383535f
somchx <90122247+somchx@users.noreply.github.com>
2022-10-10 19:10:48 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-audio-recorder-gjzks3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-audio-recorder-gjzks3)
---
+++ src/app/app.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnDestroy } from "@angular/core";
+import { AudioRecordingService } from "./audio-recording.service";
+import { DomSanitizer } from "@angular/platform-browser";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent implements OnDestroy {
+  isRecording = false;
+  recordedTime;
+  blobUrl;
+  teste;
+
+  constructor(
+    private audioRecordingService: AudioRecordingService,
+    private sanitizer: DomSanitizer
+  ) {
+    this.audioRecordingService
+      .recordingFailed()
+      .subscribe(() => (this.isRecording = false));
+    this.audioRecordingService
+      .getRecordedTime()
+      .subscribe(time => (this.recordedTime = time));
+    this.audioRecordingService.getRecordedBlob().subscribe(data => {
+      this.teste = data;
+      this.blobUrl = this.sanitizer.bypassSecurityTrustUrl(
+        URL.createObjectURL(data.blob)
+      );
+    });
+  }
+
+  startRecording() {
+    if (!this.isRecording) {
+      this.isRecording = true;
+      this.audioRecordingService.startRecording();
+    }
+  }
+
+  abortRecording() {
+    if (this.isRecording) {
+      this.isRecording = false;
+      this.audioRecordingService.abortRecording();
+    }
+  }
+
+  stopRecording() {
+    if (this.isRecording) {
+      this.audioRecordingService.stopRecording();
+      this.isRecording = false;
+    }
+  }
+
+  clearRecordedData() {
+    this.blobUrl = null;
+  }
+
+  ngOnDestroy(): void {
+    this.abortRecording();
+  }
+
+  download(): void {
+    const url = window.URL.createObjectURL(this.teste.blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = this.teste.title;
+    link.click();
+  }
+}
